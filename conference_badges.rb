5fbef24a6582d2fca685453f2dfa9f7185a15b7c
Scott Hall <scotthall127@gmail.com>
2018-07-15 18:46:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def assign_rooms(list)
   assign_rooms = []
   x = 1
   list.each do |name|
-    assign_rooms << "Hello, #{name}! You'll be assigned to room x!"
+    assign_rooms << "Hello, #{name}! You'll be assigned to room #{x}!"
     x =+ 1
   end
   assign_rooms
